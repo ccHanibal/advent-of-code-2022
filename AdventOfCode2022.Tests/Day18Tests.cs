3217eac2417c28bf7f7a5043f820372d9d347b2a
ccHanibal <oliver.daum@codecentric.de>
2022-12-18 14:05:19 +0100

    Day 18 Part 1

---
+++ AdventOfCode2022.Tests/Day18Tests.cs
@@ -0,0 +1,120 @@
+﻿using static AdventOfCode2022.Day18;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestFixture]
+	public class Day18Tests
+	{
+		private static readonly string SimpleSampleInput =
+            """
+            1,1,1
+            2,1,1
+            """;
+
+        private static readonly string SampleInput =
+            """
+            2,2,2
+            1,2,2
+            3,2,2
+            2,1,2
+            2,3,2
+            2,2,1
+            2,2,3
+            2,2,4
+            2,2,6
+            1,2,5
+            3,2,5
+            2,1,5
+            2,3,5
+            """;
+
+        [Test]
+		public void Day18_SimpleSample_SurfaceArea_Is_10()
+		{
+			var cubes = ParsePoints(SimpleSampleInput.Split("\r\n")).ToHashSet();
+			var surfaceArea = GetSurfaceArea(cubes, false);
+
+			Assert.That(surfaceArea, Is.EqualTo(10));
+		}
+
+        [Test, Explicit]
+        [TestCaseSource(nameof(AllSampleLineIndices))]
+        public void Day18_Sample_Parser_IsCorrect_ForLine(int lineIdx)
+        {
+            var inputLines = SampleInput.Split("\r\n");
+
+            var cubes = ParsePoints(inputLines);
+            Assert.That($"{cubes[lineIdx].X},{cubes[lineIdx].Y},{cubes[lineIdx].Z}", Is.EqualTo(inputLines[lineIdx]));
+        }
+        public static IEnumerable<TestCaseData> AllSampleLineIndices
+        {
+            get
+            {
+                var inputLines = SampleInput.Split("\r\n");
+
+                for (int a = 0; a < inputLines.Length; a++)
+                {
+                    yield return new TestCaseData(a);
+                }
+            }
+        }
+
+        [Test, Explicit]
+        [TestCaseSource(nameof(AllPuzzle1LineIndices))]
+        public void Day18_Puzzle1_Parser_IsCorrect_ForLine(int lineIdx)
+        {
+            var inputLines = File.ReadAllLines("Day18.txt");
+
+            var cubes = ParsePoints(inputLines);
+            Assert.That($"{cubes[lineIdx].X},{cubes[lineIdx].Y},{cubes[lineIdx].Z}", Is.EqualTo(inputLines[lineIdx]));
+        }
+        public static IEnumerable<TestCaseData> AllPuzzle1LineIndices
+        {
+            get
+            {
+                var inputLines = File.ReadAllLines("Day18.txt");
+
+                for (int a = 0; a < inputLines.Length; a++)
+                {
+                    yield return new TestCaseData(a);
+                }
+            }
+        }
+
+        [Test]
+        public void Day18_Sample_SurfaceArea_Is_64()
+        {
+            var cubes = ParsePoints(SampleInput.Split("\r\n")).ToHashSet();
+            var surfaceArea = GetSurfaceArea(cubes, false);
+
+            Assert.That(surfaceArea, Is.EqualTo(64));
+        }
+
+        [Test]
+		public async Task Day18_Puzzle1_SurfaceArea_Is_4418()
+		{
+			var cubes = ParsePoints(await File.ReadAllLinesAsync("Day18.txt")).ToHashSet();
+			var surfaceArea = GetSurfaceArea(cubes, false);
+
+            Assert.That(surfaceArea, Is.EqualTo(4_418));
+        }
+
+        [Test]
+        public void Day18_Sample_RealSurfaceArea_Is_58()
+        {
+            var cubes = ParsePoints(SampleInput.Split("\r\n")).ToHashSet();
+            var surfaceArea = GetSurfaceArea(cubes, true);
+
+            Assert.That(surfaceArea, Is.EqualTo(58));
+        }
+
+        [Test, Explicit]
+        public async Task Day18_Puzzle1_RealSurfaceArea_Is_NotSolved()
+        {
+            var cubes = ParsePoints(await File.ReadAllLinesAsync("Day18.txt")).ToHashSet();
+            var surfaceArea = GetSurfaceArea(cubes, true);
+
+            Assert.That(surfaceArea, Is.EqualTo(-1));
+        }
+    }
+}
---
+++ AdventOfCode2022/Day18.cs
@@ -0,0 +1,109 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace AdventOfCode2022
+{
+    public static class Day18
+    {
+        [DebuggerDisplay("X: {X}, Y: {Y}, Z: {Z}")]
+        public sealed class Point3D : IEquatable<Point3D>
+        {
+            public int X { get; }
+            public int Y { get; }
+            public int Z { get; }
+
+            public Point3D(int x, int y, int z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public Point3D WithOffset(Point3D offset)
+            {
+                return new Point3D(X + offset.X, Y + offset.Y, Z + offset.Z);
+            }
+            public Point3D WithOffset(int offsetX, int offsetY, int offsetZ)
+            {
+                return new Point3D(X + offsetX, Y + offsetY, Z + offsetZ);
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return Equals(obj as Point3D);
+            }
+            public override int GetHashCode()
+            {
+                return X ^ Y ^ Z;
+            }
+
+            public bool Equals(Point3D? other)
+            {
+                if (ReferenceEquals(this, other))
+                    return true;
+
+                if (other == null)
+                    return false;
+
+                return this.X == other.X && this.Y == other.Y && this.Z == other.Z;
+            }
+        }
+        public static List<Point3D> ParsePoints(IEnumerable<string> data)
+        {
+            return data.Select(ParsePoint3D)
+                        .ToList();
+
+            static Point3D ParsePoint3D(string line)
+            {
+                var parts = line.Split(',');
+                return new Point3D(
+                            int.Parse(parts[0]),
+                            int.Parse(parts[1]),
+                            int.Parse(parts[2]));
+            }
+        }
+
+        public static int GetSurfaceArea(HashSet<Point3D> points, bool includeEnclosedCubeSides)
+        {
+            int surfaceArea = 0;
+
+            var offsets = new Point3D[]
+            {
+                new Point3D(0, 0, 1),
+                new Point3D(0, 0, -1),
+                new Point3D(0, 1, 0),
+                new Point3D(0, -1, 0),
+                new Point3D(1, 0, 0),
+                new Point3D(-1, 0, 0)
+            };
+
+            foreach (var point in points)
+            {
+                foreach (var offset in offsets)
+                {
+                    var p = point.WithOffset(offset);
+
+                    var cubeFilled = points.Contains(p);
+                    if (!cubeFilled && (!includeEnclosedCubeSides || !IsEnclosedByCubes(p)))
+                        surfaceArea++;
+                }
+            }
+
+            bool IsEnclosedByCubes(Point3D testCube)
+            {
+                foreach (var offset in offsets)
+                {
+                    var p = testCube.WithOffset(offset);
+
+                    var cubeFilled = points.Contains(p);
+                    if (!cubeFilled)
+                        return false;
+                }
+
+                return true;
+            }
+
+            return surfaceArea;
+        }
+    }
+}
