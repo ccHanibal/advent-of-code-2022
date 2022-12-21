41e5e223a27fa200047605ff9e34607a9a7d7735
Oliver Daum <oliver.daum@codecentric.de>
2022-12-21 18:12:31 +0100

    Day 20

---
+++ AdventOfCode2022.Tests/Day20Tests.cs
@@ -0,0 +1,59 @@
+﻿using static AdventOfCode2022.Day20;
+
+namespace AdventOfCode2022.Tests
+{
+	[TestFixture]
+	public class Day20Tests
+	{
+		private static readonly string SampleInput =
+			"""
+			1
+			2
+			-3
+			3
+			-2
+			0
+			4
+			""";
+
+		[Test]
+		public void Day20_Sample_Mixing_Sum_Is_3()
+		{
+			var values = ParseInput(SampleInput.Split("\r\n"), 1);
+			MixValues(values);
+			var sumOfCoords = GetSum(values);
+
+			Assert.That(sumOfCoords, Is.EqualTo(3L));
+		}
+
+		[Test]
+		public async Task Day20_Puzzle1_Mixing_Sum_Is_988()
+		{
+			var values = ParseInput(await File.ReadAllLinesAsync("Day20.txt"), 1);
+			MixValues(values);
+			var sumOfCoords = GetSum(values);
+
+			Assert.That(sumOfCoords, Is.EqualTo(988L));
+		}
+
+		[Test]
+		public void Day20_Sample_Multiplied_Mixing_10_Times_Sum_Is__811_589_153()
+		{
+			var values = ParseInput(SampleInput.Split("\r\n"), 811_589_153L);
+			MixValues(values, 10);
+			var sumOfCoords = GetSum(values);
+
+			Assert.That(sumOfCoords, Is.EqualTo(1_623_178_306L));
+		}
+
+		[Test]
+		public async Task Day20_Puzzle1_Multiplied_Mixing_10_Times_Sum_Is_7_768_531_372_516()
+		{
+			var values = ParseInput(await File.ReadAllLinesAsync("Day20.txt"), 811_589_153L);
+			MixValues(values, 10);
+			var sumOfCoords = GetSum(values);
+
+			Assert.That(sumOfCoords, Is.EqualTo(7_768_531_372_516L));
+		}
+	}
+}
---
+++ AdventOfCode2022/Day20.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode2022
+{
+	public static class Day20
+	{
+		public record Data(int Index, long Value);
+
+		public static List<Data> ParseInput(IEnumerable<string> data, long m)
+		{
+			return data.Select((v, idx) => new Data(idx, long.Parse(v) * m)).ToList();
+		}
+
+		public static long GetSum(List<Data> values)
+		{
+			return Enumerate(0, 1000, 2000, 3000)
+						.Sum(i => i);
+
+			IEnumerable<long> Enumerate(long targetValue, int index1, int index2, int index3)
+			{
+				int targetIndex = values.FindIndex(v => v.Value == targetValue);
+
+				yield return values[(targetIndex + index1) % values.Count].Value;
+				yield return values[(targetIndex + index2) % values.Count].Value;
+				yield return values[(targetIndex + index3) % values.Count].Value;
+			}
+		}
+
+		public static List<Data> MixValues(List<Data> values, int times)
+		{
+			for (int a = 0; a < times; a++)
+			{
+				MixValues(values);
+			}
+
+			return values;
+		}
+		public static List<Data> MixValues(List<Data> values)
+		{
+			var mod = values.Count - 1;
+
+			for (int idx = 0; idx < values.Count; idx++)
+			{
+				var index = values.FindIndex(v => v.Index == idx);
+				var num = values[index];
+				if (num.Value == 0)
+					continue;
+
+				var targetIndex = (index + num.Value) % mod;
+				while (targetIndex < 0)
+					targetIndex += mod;
+
+				values.RemoveAt(index);
+				values.Insert((int)targetIndex, num);
+			}
+
+			return values;
+		}
+	}
+}
